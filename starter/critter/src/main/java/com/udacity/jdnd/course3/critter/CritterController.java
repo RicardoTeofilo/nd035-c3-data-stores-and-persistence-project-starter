fe6b5b4f49063cb0a182c67a575e52fe46d54ab8
RicardoTeofilo <ricardopachec38@gmail.com>
2023-02-24 14:43:11 -0300

    Reenabling the dummy controller.

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/CritterController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/CritterController.java
@@ -1,12 +1,13 @@
 package com.udacity.jdnd.course3.critter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Dummy controller class to verify installation success. Do not use for
  * your project work.
  */
-//@RestController
+@RestController
 public class CritterController {
 
     @GetMapping("/test")
